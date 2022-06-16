c89cf74d5f05abffb07eeb4631b847f6e39dfd66
Adeoyetestimony <testimonyoluwapelumi@gmail.com>
2022-06-16 13:04:22 +0100

    quiz app with grade

---
+++ app.js
@@ -0,0 +1,42 @@
+const correctAnswer = ['B', 'B', 'B', 'B'];
+const form = document.querySelector ('.quiz-form');
+const result = document.querySelector ('.result');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    let score = 0;
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value]
+
+    //check answers
+    userAnswers.forEach((answer, index) =>{
+        if (answer === correctAnswer[index]) {
+            score += 25;
+
+        }
+    });
+
+    // show result on page
+    scrollTo(0,0);
+
+    result.classList.remove('d-none');
+    let output = 0;
+    const timer = setInterval(() => {
+        result.querySelector('span').textContent = `${output}%`;
+        if (output === score) {
+            clearInterval(timer);
+        } else{
+            output++;
+        }
+    }, 10);
+});
+
+// window object (global object)
+// i = 0;
+// const timer = setInterval(() => {
+//     console.log('heloo');
+//     i++
+//     if (i ===5) {
+//        clearInterval(timer);
+//     }
+// }, 1000);
